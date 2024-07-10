99061e5f8d3686e8dabcd468e088c3a03a63b44b
Farhana Aktar <farhanaaktar@live.co.uk>
2024-07-10 12:40:40 +0100

    finalbackground

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ function updateDisplay() {
   display.innerText = displayValue
 }
 
+
+// make it flip with results in the back
+// change color when clicked on buttons but reverts when enter or cleared
+// create a box with memory or what numbers where clicked and a clear button to clear all that is a pop up with a icon to pop up
+// at the top do a cool font with a cool calculator name 
+
 const calculatorDisplay = document.querySelector('.calculator')
 calculatorDisplay.style.backgroundColor = 'pink'
 
 calculatorDisplay.style.boxShadow = '10px 20px 30px'
+calculatorDisplay.style.border = "thick solid black"
 
+const body = document.querySelector('body');
 
-
-
+body.style.backgroundImage = 'url("poparts.jpg")';
+body.style.backgroundRepeat = 'no-repeat';
+body.style.backgroundSize = 'cover';goi
 
 const buttonHover = document.querySelector('.buttons')
 buttonHover.classList.add('hover')
